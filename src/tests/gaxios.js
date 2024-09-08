cdc0e68bf723e0dec14d6958bd1f3b601c8f7da6
homer0 <me@homer0.dev>
2024-09-08 07:02:55 +0200

    feat: add test for gaxios

---
+++ src/tests/gaxios.js
@@ -0,0 +1,36 @@
+const gaxios = require('gaxios');
+const { HsmTest } = require('..');
+
+class AxiosTest extends HsmTest {
+  test(start, finish, reject) {
+    start();
+    gaxios
+      .request({
+        url: this.url,
+        transformResponse: [(data) => data],
+        headers: {
+          'User-Agent': this.userAgent,
+        },
+      })
+      .then(() => finish())
+      .catch((error) => reject(error));
+  }
+
+  testJSON(start, finish, reject) {
+    start();
+    gaxios
+      .request({
+        url: this.url,
+        headers: {
+          'User-Agent': this.userAgent,
+        },
+      })
+      .then(() => finish())
+      .catch((error) => reject(error));
+  }
+  get name() {
+    return 'gaxios';
+  }
+}
+
+new AxiosTest().run();
