1ac9bffa981f31439ccaaf04a8fc4fa9a0ca486e
homer0 <me@homer0.dev>
2024-09-08 06:47:14 +0200

    feat: add test for native fetch

---
+++ src/tests/fetch.js
@@ -0,0 +1,32 @@
+const { HsmTest } = require('..');
+
+class FetchTest extends HsmTest {
+  test(start, finish, reject) {
+    start();
+    fetch(this.url, {
+      headers: {
+        'User-Agent': this.userAgent,
+      },
+    })
+      .then((response) => response.text())
+      .then(() => finish())
+      .catch((error) => reject(error));
+  }
+
+  testJSON(start, finish, reject) {
+    start();
+    fetch(this.url, {
+      headers: {
+        'User-Agent': this.userAgent,
+      },
+    })
+      .then((response) => response.json())
+      .then(() => finish())
+      .catch((error) => reject(error));
+  }
+  get name() {
+    return 'fetch';
+  }
+}
+
+new FetchTest().run();
